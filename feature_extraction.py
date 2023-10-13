2cfc4d4961b4d5361d3006fc37fcb9f67cb4634e
BelindaNTHU <146788377+BelindaNTHU@users.noreply.github.com>
2023-10-13 13:55:19 +0800

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,55 +6,27 @@ Created on Tue Oct  3 07:59:29 2023
 """
 
 import librosa
-audio_data = 'road1.mp3'
+audio_data = 'audio/ambulance1.mp3'
 x , sr = librosa.load(audio_data)
-print(type(x), type(sr))# print(x.shape, sr)#(94316,) 22050
+print(x.shape, sr)#(220207,) 22050
 librosa.load(audio_data, sr=44100)
 import IPython.display as ipd
 ipd.Audio(audio_data)
 
+###############################################
+
 import matplotlib.pyplot as plt
+import matplotlib
 import librosa.display
 plt.figure(figsize=(14, 5))
 librosa.display.waveshow(x, sr=sr)
+plt.savefig("time_domain/timeD_ambulance1.png")
 
-X = librosa.stft(x)
+
+X = librosa.stft(x) #stft轉換的function，可以用參數調整
 Xdb = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') #y_axis='log'
-plt.colorbar()
-     
-#Plot the signal:
-plt.figure(figsize=(14, 5))
-librosa.display.waveshow(x, sr=sr)
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') #photo2 經stft轉換後的圖 我覺得y_axis='log'的效果比'hz'好
+plt.savefig("frequency/frequencyD_ambulance1.png")
 
-# Zooming in
-n0 = 9000
-n1 = 9100
-plt.figure(figsize=(14, 5))
-plt.plot(x[n0:n1])
-plt.grid()
 
-#spectral centroid -- centre of mass -- weighted mean of the frequencies present in the sound
-import sklearn
-from sklearn import preprocessing
-spectral_centroids = librosa.feature.spectral_centroid(y=x, sr=sr)[0]
-spectral_centroids.shape
-# Computing the time variable for visualization
-frames = range(len(spectral_centroids))
-t = librosa.frames_to_time(frames)
-# Normalising the spectral centroid for visualisation
-def normalize(x, axis=0):
-    return sklearn.preprocessing.minmax_scale(x, axis=axis)
-#Plotting the Spectral Centroid along the waveform
-librosa.display.waveshow(x, sr=sr, alpha=0.4)
-plt.plot(t, normalize(spectral_centroids), color='r')
-
-spectral_rolloff = librosa.feature.spectral_rolloff(y=x, sr=sr)[0]
-librosa.display.waveshow(x, sr=sr, alpha=0.4)
-plt.plot(t, normalize(spectral_rolloff), color='r')
-mfccs = librosa.feature.mfcc(y=x, sr=sr)
-print(mfccs.shape)
-
-#Displaying  the MFCCs:
-librosa.display.specshow(mfccs, sr=sr, x_axis='time')
