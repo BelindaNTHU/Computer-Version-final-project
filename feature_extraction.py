7e761f01334e6972ff5f6246faf586615ef1e412
BelindaNTHU <146788377+BelindaNTHU@users.noreply.github.com>
2023-10-13 13:58:51 +0800

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -20,13 +20,13 @@ import matplotlib
 import librosa.display
 plt.figure(figsize=(14, 5))
 librosa.display.waveshow(x, sr=sr)
-plt.savefig("time_domain/timeD_ambulance1.png")
+plt.savefig("time_domain/timeD_ambulance1.png") #儲存圖片
 
 
 X = librosa.stft(x) #stft轉換的function，可以用參數調整
 Xdb = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') #photo2 經stft轉換後的圖 我覺得y_axis='log'的效果比'hz'好
-plt.savefig("frequency/frequencyD_ambulance1.png")
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log') #photo2 經stft轉換後的圖
+plt.savefig("frequency_domain/frequencyD_ambulance1.png") #儲存圖片
 
 
